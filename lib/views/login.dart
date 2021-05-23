0eb344b4dd7cc9c2b526dd96e99abd627a95e2e9
yogesh upadhyay <yogeshh2021@gmail.com>
2021-05-23 16:06:32 +0530

    login page updated

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,7 +1,59 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:bizmanager/views/components/textfield.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.deepOrangeAccent,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: size.width*0.1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: size.width*0.6,
+              child: Image.asset(
+                "assets/images/logo.png"
+              ),
+            ),
+            TextFieldComponent(
+              prefixIcon: Icon(Icons.email), 
+              labelText: "Email", 
+              obsText: false
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFieldComponent(
+              prefixIcon: Icon(Icons.email), 
+              labelText: "Email", 
+              obsText: false
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              onPressed: () {  },
+              child: Text(
+                "Submit",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "OpenSans"
+                ),
+              ),
+              color: Colors.black,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Landing extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -10,13 +62,18 @@ class Login extends StatelessWidget {
       body: Stack(
         children: [
           Image.asset("assets/images/backdrop_upper.gif"),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset("assets/images/backdrop_lower.gif")
+          ),
           Center(
             child: Image.asset(
               "assets/images/logo.png",
               fit: BoxFit.cover,
               width: size.width*0.8,
             )
-          )
+          ), 
         ],
       )
     );
