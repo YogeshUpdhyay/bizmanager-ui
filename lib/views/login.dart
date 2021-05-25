5429c78da5a3ec57157d2afa5cccc837efc04efc
yogesh upadhyay <yogeshh2021@gmail.com>
2021-05-26 01:02:47 +0530

    login ui updated

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,5 +1,6 @@
 import 'package:bizmanager/views/components/textfield.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 class MainView extends StatelessWidget {
   @override
@@ -8,55 +9,85 @@ class MainView extends StatelessWidget {
   }
 }
 
+class RPSCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+            
+  Path path_0 = Path();
+  path_0.moveTo(size.width,0);
+  path_0.lineTo(0,0);
+  path_0.lineTo(size.width*0.7435417,size.height*0.9560000);
+  path_0.lineTo(size.width,0);
+  path_0.close();
+
+  Paint paint_0_fill = Paint()..style=PaintingStyle.fill;
+  paint_0_fill.color = Colors.black;
+  canvas.drawPath(path_0,paint_0_fill);
+
+}
+
+@override
+bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+}
+}
+
 class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
+    final WIDTH = size.width;
+
     return Scaffold(
-      backgroundColor: Colors.deepOrangeAccent,
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: size.width*0.1),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: size.width*0.6,
-              height: size.height*0.3,
-              child: Image.asset(
-                "assets/images/logo.png"
-              ),
-            ),
-            TextFieldComponent(
-              prefixIcon: Icons.email, 
-              labelText: "Email", 
-              obsText: false
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFieldComponent(
-              prefixIcon: Icons.password_rounded, 
-              labelText: "Password", 
-              obsText: true
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            MaterialButton(
-              onPressed: () {  },
-              child: Text(
-                "Submit",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: "OpenSans"
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          CustomPaint(
+              size: Size(WIDTH, (size.height*0.4).toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+              painter: RPSCustomPainter(),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: size.width*0.05),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold
+                  ),
                 ),
-              ),
-              color: Colors.black,
-            )
-          ],
-        ),
+                SizedBox(height: 10.0,),
+                TextFieldComponent(prefixIcon: Icons.email_rounded, labelText: "Email", obsText: false),
+                SizedBox(height: 10.0,),
+                TextFieldComponent(prefixIcon: Icons.lock, labelText: "Password", obsText: true),
+                SizedBox(height: 10.0),
+                MaterialButton(
+                  onPressed: () {}, 
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(
+                      fontFamily: "OpenSans",
+                      fontSize: 20,
+                      color: Colors.white
+                    ),
+                  ),
+                  color: Colors.black,
+                  height: size.height*0.06,
+                  minWidth: size.width*0.9,
+                  elevation: 10.0,
+
+                ),
+                SizedBox(height: 20,),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
