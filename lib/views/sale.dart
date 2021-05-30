a9f39542463f6b61af72109ff8bbff08051dfdf5
yogesh upadhyay <yogeshh2021@gmail.com>
2021-05-30 23:54:29 +0530

    sales view updated

--- lib/views/sale.dart
+++ lib/views/sale.dart
@@ -1,12 +1,79 @@
+import 'package:bizmanager/views/parties.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
-class Sales extends StatelessWidget {
+Future<List<DropdownMenuItem<String>>?> getParties()  async {
+  
+  try {
+    var parties = <DropdownMenuItem<String>>[];
+    var dio = Dio();
+    var response = await dio.get("http://10.0.2.2:8000/api/v1/parties");
+    
+    var data = response.data;
+    
+    for (var i = 0; i < data.length; i++) {
+      parties.add(
+        DropdownMenuItem<String>(
+          child: Text(data[i]),
+          value: data[i],
+        )
+      );
+    }
+    print(parties);
+    return parties;
+  } catch (error) {
+    return null;
+  }
+}
+
+class Sales extends StatefulWidget {
+  @override
+  _SalesState createState() => _SalesState();
+}
+
+class _SalesState extends State<Sales> {
+  
+  var parties;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getParties()
+    .then((value) => {
+      // print(value),
+      setState(() => parties = value),
+      print("parties ${parties}")
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        "Sales View"
+    
+    final size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: size.width*0.05),
+          child: Column(
+            children: [
+              SizedBox(height: 10.0,),
+              DropdownButton(
+                value: "Party1",
+                items: parties,
+                hint: Text("Parties",  style: TextStyle(fontFamily: "OpenSans"),),
+                isExpanded: true,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
+
 }
